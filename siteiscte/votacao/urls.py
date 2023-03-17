50e8828c188fdad0183bca4370207e12887426f2
MLValadares <mlvss@iscte-iul.pt>
2023-03-17 10:31:52 +0000

    criaropcao

--- siteiscte/votacao/urls.py
+++ siteiscte/votacao/urls.py
@@ -11,10 +11,10 @@ path('<int:questao_id>', views.detalhe, name='detalhe'),
 path('<int:questao_id>/resultados', views.resultados, name='resultados'),
 # ex: votacao/5/voto
 path('<int:questao_id>/voto', views.voto, name='voto'),
-# votacao/1/criaropcao
-path('<int:questao_id>/criaropcao', views.criaropcao, name='criaropcao'),
 # votacao/criarquestao
 path('criarvotacao', views.criarquestao, name='criarquestao'),
 # votacao/createquestion
-path('createquestion', views.createquestion, name='createquestion')
+path('createquestion', views.createquestion, name='createquestion'),
+# votacao/1/criaropcao
+path('<int:questao_id>/criaropcao', views.criaropcao, name='criaropcao')
 ]
