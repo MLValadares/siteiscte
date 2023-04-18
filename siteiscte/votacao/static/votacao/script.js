eff50c6442153c1163904a2196c374274a0d594d
MLValadares <mlvss@iscte-iul.pt>
2023-04-18 17:28:49 +0100

    1 - exc feito

---
+++ siteiscte/votacao/static/votacao/script.js
@@ -0,0 +1,8 @@
+function hidePhoto(){
+    var x = document.getElementById("fperfil")
+    if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
