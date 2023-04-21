b97bc45292797ec69c20a8f3adf0bfaa92fe2d69
MLValadares <mlvss@iscte-iul.pt>
2023-04-21 17:57:51 +0100

    eeeeeeeeee

--- siteiscte/votacao/static/votacao/script.js
+++ siteiscte/votacao/static/votacao/script.js
@@ -21,3 +21,27 @@ function lista(){
         x.style.display = "none";
     }
 }
+$("botao_validar").click(function(){
+    alert("fqf")
+    // var texto = $('#comentario').val();
+    // var palavras = texto.split(" ");
+    // arrayInsultos = ["abécula", "abentesma", "achavascado", "alimária", "andrajoso", "barregã", "biltre", "cacóstomo", "cuarra", "estólido", "estroso", "estultilóquio", "nefelibata", "néscio", "pechenga", "sevandija", "somítico", "tatibitate", "xexé", "cheché", "xexelento"]
+    // var existeInsulto = new Boolean(false);
+    // var insulto = 'nenhum';
+    // for (var i = 0; i < palavras.length; i++) {
+    //     palavramin = palavras[i].toLowerCase();
+    //     for (var z = 0; z < arrayInsultos.length; z++) {
+    //         if (palavramin === arrayInsultos[z]) {
+    //             existeInsulto = new Boolean(true);
+    //             insulto = palavramin;
+    //             break;
+    //         }
+    //     }
+    // }
+    // if (existeInsulto == true){
+    //     //alert(insulto);
+    //     $('#comentario').val("Por favor insira um comentário sem insultos.");
+    // } else {
+    //     $('#alerta_comentario').text('Validado');
+    // }
+});
