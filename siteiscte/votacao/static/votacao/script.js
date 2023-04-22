46e8e00659ebf956ff623179fa5781f0b9c765cf
MLValadares <mlvss@iscte-iul.pt>
2023-04-22 10:54:08 +0100

    4 - ex feito?

--- siteiscte/votacao/static/votacao/script.js
+++ siteiscte/votacao/static/votacao/script.js
@@ -21,27 +21,32 @@ function lista(){
         x.style.display = "none";
     }
 }
-$("botao_validar").click(function(){
-    alert("fqf")
-    // var texto = $('#comentario').val();
-    // var palavras = texto.split(" ");
-    // arrayInsultos = ["abécula", "abentesma", "achavascado", "alimária", "andrajoso", "barregã", "biltre", "cacóstomo", "cuarra", "estólido", "estroso", "estultilóquio", "nefelibata", "néscio", "pechenga", "sevandija", "somítico", "tatibitate", "xexé", "cheché", "xexelento"]
-    // var existeInsulto = new Boolean(false);
-    // var insulto = 'nenhum';
-    // for (var i = 0; i < palavras.length; i++) {
-    //     palavramin = palavras[i].toLowerCase();
-    //     for (var z = 0; z < arrayInsultos.length; z++) {
-    //         if (palavramin === arrayInsultos[z]) {
-    //             existeInsulto = new Boolean(true);
-    //             insulto = palavramin;
-    //             break;
-    //         }
-    //     }
-    // }
-    // if (existeInsulto == true){
-    //     //alert(insulto);
-    //     $('#comentario').val("Por favor insira um comentário sem insultos.");
-    // } else {
-    //     $('#alerta_comentario').text('Validado');
-    // }
+
+//Note to self: Ainda não percebi como isto funcionou
+//Fiz pip do seguinto pacote: https://pypi.org/project/django-jquery/
+//E adicionei 'jquery' no INSTALLED_APPS no settings.py, não estava a conseguir via import "{{ STATIC_URL }}js/jquery.js"
+//Como aparece no link a cima
+//No entanto, voltei a exprimentar com "https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js" e funcionou
+//
+$("#btt1").click(function(){
+    var texto = $('#comentario').val();
+    var palavras = texto.split(" ");
+    arrayInsultos = ["abécula", "abentesma", "achavascado", "alimária", "andrajoso", "barregã", "biltre", "cacóstomo", "cuarra", "estólido", "estroso", "estultilóquio", "nefelibata", "néscio", "pechenga", "sevandija", "somítico", "tatibitate", "xexé", "cheché", "xexelento"]
+    var existeInsulto = new Boolean(false);
+    var insulto = 'nenhum';
+    for (var i = 0; i < palavras.length; i++) {
+        palavramin = palavras[i].toLowerCase();
+        for (var z = 0; z < arrayInsultos.length; z++) {
+            if (palavramin === arrayInsultos[z]) {
+                existeInsulto = new Boolean(true);
+                insulto = palavramin;
+                break;
+            }
+        }
+    }
+    if (existeInsulto == true){
+        $('#comentario').val("Por favor insira um comentário sem insultos.");
+    } else {
+        $('#alerta_comentario').text('Validado');
+    }
 });
